da52eede78790241a536b53c4b60c632c1354bd9
Zay Zay <67672136+zayzay4mm@users.noreply.github.com>
2023-08-01 20:27:54 +0630

    Update conn.inc.php

--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -1,8 +1,8 @@
 <?php
 $hostname = "127.0.0.1";
-$user = "root";
-$password = "1234";
-$database = "blog";
+$user = "";
+$password = "";
+$database = "";
 $conn = new mysqli($hostname,$user,$password,$database);
 if ($conn->connect_error) {
    echo "Can't connect to the database!";
